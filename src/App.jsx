507cae3d9e89e17f4fcd8f4065db79340dc8c332
name <kcisha675@gmail.com>
2026-03-28 13:11:15 -0500

    Add landing page and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import Dashboard from './pages/Dashboard'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-  return <Dashboard />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"          element={<LandingPage />} />
+        <Route path="/home"      element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
